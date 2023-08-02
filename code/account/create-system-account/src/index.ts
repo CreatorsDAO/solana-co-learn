6477af136a7da708ac569b138f10d49ac53cf965
Davirain <davirain.yin@gmail.com>
2023-08-02 09:33:22 +0800

    Add program and account

---
+++ code/account/create-system-account/src/index.ts
@@ -0,0 +1,11 @@
+async function main() {}
+
+main()
+    .then(() => {
+        console.log("Finished successfully")
+        process.exit(0)
+    })
+    .catch((error) => {
+        console.log(error)
+        process.exit(1)
+    })
