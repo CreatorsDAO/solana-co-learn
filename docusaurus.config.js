bb57aeca6029d5a87d86e3290b091e6243268663
Davirain <davirain.yin@gmail.com>
2023-09-03 18:05:17 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,7 +22,7 @@ function defineSection(section, version = {}, options = {}) {
       include: ['**/*.md', '**/*.mdx'],
       breadcrumbs: false,
       sidebarPath: require.resolve('./sidebars.cjs'),
-      editUrl: 'https://github.com/CreatorsDAO/all-in-one-solana/edit/main',
+      editUrl: 'https://github.com/CreatorsDAO/all-in-one-solana/tree/dev',
       versions: version && {
         current: {
           label: version.label,
